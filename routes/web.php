c72353c19d25149ef3f56882dd3239ed02e50a67
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-03-25 20:00:55 +0100

    add link route with page controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Guest\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-  return view('home');
-})->name('home');
+// Page controller
+Route::get('/', [PageController::class,'index'])->name('home');

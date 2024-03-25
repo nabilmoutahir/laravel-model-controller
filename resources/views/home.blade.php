f3f2583c997d55347744ac3eadd69f3c16f336cb
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-03-25 20:35:45 +0100

    add layout and display database elements

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,27 @@
 @section('title', 'Pagina iniziale')
 
 @section('main-content')
-  <section>
-    <div class="container py-4">
-      <h1>Laravel with Bootstrap + Vite</h1>
+  
+
+  <div class="row">
+
+    @foreach ($movies as $movie)
+    <div class="col-6">
+
+      <div class=" card">
+        <div class=" card-body ">
+          <p>{{$movie -> id}}</p>
+          <p>{{$movie -> title}}</p>
+          <p>{{$movie -> original_title}}</p>
+          <p>{{$movie -> nationality}}</p>
+          <p>{{$movie -> date}}</p>
+          <p>{{$movie -> vote}}</p>
+        </div>
+      </div>
+
     </div>
-  </section>
+    @endforeach
+    
+  </div>
+
 @endsection
